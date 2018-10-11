3326cacc9e59b6cac6c9e65a98b43a3af9897fc8
tyy <451655144@qq.com>
2018-10-11 11:47:33 +0800

    Initial commit

---
+++ src/main/java/com/tuling/Hello.java
@@ -0,0 +1,8 @@
+package com.tuling;
+public class Hello{
+	public String fun(){
+		return "success";
+	}
+
+}
+
---
+++ src/test/java/com/tuling/test/TestHello.java
@@ -0,0 +1,10 @@
+package com.tuling.test;
+import org.junit.Test;
+import org.junit.Assert;
+public class TestHello{
+	@Test
+	public void testSayhello(){
+		System.out.println("run test.....");
+		Assert.assertEquals("1","1");
+	}
+}
